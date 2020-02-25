52379b04923364720362fb274da55dad63dac0e9
Spencer <spencerwong24@gmail.com>
2020-02-25 15:21:10 -0800

    Initial Commit

---
+++ SpartanDrive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpartanDrive
+//
+//  Created by Spencer on 2/25/20.
+//  Copyright © 2020 Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
