c7c3269578e15f4062d1b181eae0ff7de52412cd
Spencer <spencerwong24@gmail.com>
2020-02-26 09:19:10 -0800

    integrate working uikit

--- SpartanDrive/ViewController.swift
+++ SpartanDrive/ViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var count = 0
+    @IBOutlet weak var countLabel: UILabel!
+    @IBAction func countButton(_ sender: Any) {
+        count += 1
+        countLabel.text = "Count: \(count)"
     }
-
-
 }
 
